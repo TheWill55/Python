b7c93ca4ff77ae3e7fd2b33939fe047e7310c988
Wilson Alves <127157385+TheWill55@users.noreply.github.com>
2023-07-02 12:03:57 -0300

    Update ex03.py

--- EX000/ex03.py
+++ EX000/ex03.py
@@ -1,7 +1,7 @@
-num1 = int(input('Digite o primeiro número:'))
-num2 = int(input('Digite o segundo número:'))
-soma = num1 + num2
-print('A soma entre {} e {} vale {}'.format( num1, num2, soma))
+valor = int(input('Digite um valor:'))
+valor2 = int(input('Digite outro valor:'))
+soma = valor + valor2
+print('A soma entre o valor {} e o valor {} é de {}!'.format(valor,valor2,soma))
 
 
 
