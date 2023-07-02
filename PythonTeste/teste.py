77df99742179d0c01ea7672a38b5fcfce003471f
Wilson Alves <127157385+TheWill55@users.noreply.github.com>
2023-07-02 15:00:06 -0300

    Update teste.py

--- PythonTeste/teste.py
+++ PythonTeste/teste.py
@@ -1,2 +1,14 @@
-n = input('Digite algo; ')
-print(n.isalnum())
+# Definição da classe Pessoa
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def cumprimentar(self):
+        print(f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos.")
+
+# Criando um objeto Pessoa
+pessoa1 = Pessoa("João", 25)
+
+# Chamando o método cumprimentar do objeto pessoa1
+pessoa1.cumprimentar()
