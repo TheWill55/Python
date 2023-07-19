b92516ef756fc42889be83a15751a6ab6c18b5e5
Wilson Alves <127157385+TheWill55@users.noreply.github.com>
2023-07-18 21:04:58 -0300

    teste07

---
+++ PythonTeste/teste07.py
@@ -0,0 +1,14 @@
+print('===Aparelho: Xiaomi mi 13===')
+print('   Valor: R$ 1846.54 ')
+din = float(input('Dinheiro na conta:'))
+cell = 1846.54
+print('Dinehiro em conta é R${:.2f}'.format(din))
+if din >= cell:
+    print('Compra altorizada!')
+    sob = din - cell
+    print('Total restante em conta é R${:.2f}'.format(sob))
+else:
+    print('Compra negada!')
+    falta = cell - din
+    print('Falta apenas R${:.2f} para a compra do aparelho'.format(falta))
+print('Agradeçemos, volte sempre!')
